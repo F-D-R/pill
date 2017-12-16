d7a44dc4677dd7c7bca6d83aed940786d5132ffb
unknown <fdr@fdr.hu>
2017-12-16 22:54:08 +0100

    Direction control

--- robot/movement.c
+++ robot/movement.c
@@ -6,7 +6,11 @@
 #define MAX_SPEED 1000
 #define MIN_SPEED -1000
 static volatile int speed = 0;
+#define MAX_DIR 180
+#define MIN_DIR -180
 static volatile int dir = 0;
+#define MAX_PWM 2000
+#define MIN_PWM 1000
 static volatile uint16_t pwm_left = 1500;
 static volatile uint16_t pwm_right = 1500;
 
@@ -15,8 +19,25 @@ void calc_output(void) {
 		speed = MAX_SPEED;
 	else if (speed < MIN_SPEED)
 		speed = MIN_SPEED;
-	pwm_left = (speed / 2) + 500;
-	pwm_right = (speed / 2) + 500;
+	
+	if (dir > MAX_DIR)
+		dir = MAX_DIR;
+	else if (dir < MIN_DIR)
+		dir = MIN_DIR;
+	
+	pwm_left = (speed / 2) + dir + 500;
+	pwm_right = (speed / 2) - dir + 500;
+	
+	if (pwm_left > MAX_PWM)
+		pwm_left = MAX_PWM;
+	else if (pwm_left < MIN_PWM)
+		pwm_left = MIN_PWM;
+	
+	if (pwm_right > MAX_PWM)
+		pwm_right = MAX_PWM;
+	else if (pwm_right < MIN_PWM)
+		pwm_right = MIN_PWM;
+	
 	set_pwm_output(pwm_left, pwm_right);
 }
 
